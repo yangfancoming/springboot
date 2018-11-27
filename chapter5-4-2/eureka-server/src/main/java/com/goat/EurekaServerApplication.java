0d33c2a4eae66215ec9d9db58530285d2cab069d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-11-27 12:01:08 +0800

    增加 springcloud 中的 eureka server

---
+++ chapter5-4-2/eureka-server/src/main/java/com/goat/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.goat;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+     * @Description: 启动后  输入  http://localhost:8542/ 即可看到 eureka 页面
+     * @author: 杨帆
+     * @Param:
+     * @Return:
+     * @Date:   2018/11/27
+*/
+@EnableEurekaServer // 启动服务注册中心
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
