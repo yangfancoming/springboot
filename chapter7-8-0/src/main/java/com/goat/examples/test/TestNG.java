da3272d13941d5ccc9dfa056e082a4a4f50521b8
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-12-04 21:43:23 +0800

    增加 drools  示例

--- chapter7-8-0/src/main/java/com/goat/examples/test/TestNG.java
+++ chapter7-8-0/src/main/java/com/goat/examples/test/TestNG.java
@@ -1,6 +1,7 @@
 package com.goat.examples.test;
 
 
+import com.goat.bean.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -25,6 +26,11 @@ public class TestNG  {
         KieContainer kieContainer = kieServices.newKieClasspathContainer(); //从KieServices中获得KieContainer实例，其会加载kmodule.xml文件并load规则文件
         //建立KieSession到规则文件的通信管道
         KieSession kieSession = kieContainer.newKieSession("test"); // 参数对应   <ksession name="helloWorldSession"/>
+
+//        Person person = new Person("duval yang",12);
+        Person person = new Person("duval yang",17);
+        kieSession.insert(person);
+
         kieSession.fireAllRules();
         kieSession.dispose();
     }
