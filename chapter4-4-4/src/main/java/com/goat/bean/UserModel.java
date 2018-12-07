2e6d3067ccd6014f6941b1c85462593f2b2e06c7
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-12-07 23:45:12 +0800

    增加 mybatis-plus  AR 操作示例

---
+++ chapter4-4-4/src/main/java/com/goat/bean/UserModel.java
@@ -0,0 +1,108 @@
+package com.goat.bean;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
+import java.io.Serializable;
+
+/**
+ * Created by 64274 on 2018/11/13.
+ *
+ * @author 山羊来了
+ * @Description: TODO
+ * @date 2018/11/13---13:29
+ */
+
+@TableName("userModel")
+public class UserModel extends Model<UserModel> {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    @TableField(exist = false)
+    private String test;
+    private Integer version;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserModel() {
+    }
+
+    // 注定实体类的主键属性
+    @Override
+    protected Serializable pkVal() {
+        return id;
+    }
+
+    public UserModel(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UserModel(Long id, String name, Integer version) {
+        this.id = id;
+        this.name = name;
+        this.version = version;
+    }
+
+    public UserModel(Long id, String name, Integer age, Integer version) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.version = version;
+    }
+
+    public UserModel(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public UserModel(String name, Integer age, String test) {
+        this.name = name;
+        this.age = age;
+        this.test = test;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + ", test='" + test + '\'' + ", version=" + version + '}';
+    }
+}
---
+++ chapter4-4-4/src/test/java/com/goat/ar/ARTest.java
@@ -0,0 +1,112 @@
+package com.goat.ar;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.goat.bean.User;
+import com.goat.bean.UserModel;
+import com.goat.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * Created by 64274 on 2018/11/13.
+ *
+ * @author 山羊来了
+ * @Description: TODO
+ * @date 2018/11/13---13:28
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ARTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void insert() {
+        UserModel userModel = new UserModel();
+        userModel.setName("vander");
+        userModel.setAge(24);
+        userModel.insert(); //-----------------  字段不为空插入
+    }
+    @Test
+    public void insertOrUpdate() {
+        UserModel userModel = new UserModel();
+        userModel.setName("vander");
+        userModel.setAge(24);
+        userModel.insertOrUpdate(); //-----------------  ID为空插入，否则为更新
+    }
+
+
+    @Test
+    public void updateById() { //-----------------  ID 修改
+        UserModel userModel = new UserModel();
+        userModel.setName("vander");
+        userModel.setAge(24);
+        userModel.updateById();
+    }
+
+    @Test
+    public void update() { //-----------------  条件修改
+        UserModel userModel = new UserModel();
+        userModel.setName("vander");
+        userModel.setAge(24);
+        /**
+         * UPDATE tbl_employee SET last_name='vander',email='vander-4@foxmail.com',gender=0,age=24 WHERE gender = '5'
+         */
+        userModel.update(new UpdateWrapper<UserModel>().eq("gender","5"));
+    }
+
+    @Test
+    public void ARSelect() {
+        UserModel userModel = new UserModel();
+        userModel.setId(24L);
+
+        //-----------------  setId
+        userModel.selectById();
+
+        //-----------------  直接键入 Id
+        userModel.selectById(24);
+
+        //-----------------  条件
+        userModel.selectCount(new QueryWrapper<>().eq("gender", "0"));
+
+        //-----------------  查询所有
+        userModel.selectAll();
+
+        //-----------------  查询总记录数
+        userModel.selectList((Wrapper) new QueryWrapper().eq("gender", 0));
+
+        //-----------------
+        userModel.selectOne(new QueryWrapper());
+
+        //-----------------
+        userModel.selectPage(new Page<>(1,2), new QueryWrapper<>());
+
+    }
+
+
+    @Test
+    public void ARDelete() {
+        /**
+         *  删除不存在的数据 在逻辑上也是成功的，返回结果 true
+         */
+        UserModel userModel = new UserModel();
+        userModel.setId(30L);
+
+        //-----------------
+        userModel.deleteById();
+
+        //-----------------
+        userModel.deleteById(31);
+
+        //-----------------
+        userModel.delete((Wrapper) new QueryWrapper().eq("Id", 32));
+    }
+}
