637fda329226d8d40faea01a06d0ccf4dec2f501
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-12-13 14:30:34 +0800

    增加 JavaSE I/O  reader 和 writer 使用示例

---
+++ chapter99-15-3/src/main/java/com/goat/A03/FileReaderTest.java
@@ -0,0 +1,29 @@
+package com.goat.A03;
+
+import org.testng.annotations.Test;
+
+import java.io.*;
+
+
+public class FileReaderTest {
+
+    @Test
+    public void test(){
+        try (
+                // 创建字符输入流
+                FileReader fr = new FileReader("E:\\Code\\J2EE_code\\MySpringBoot\\springboot\\chapter99-15-3\\src\\main\\java\\com\\goat\\A03\\FileWriterTest.java")) {
+            // 创建一个长度为32的“竹筒”
+            char[] cbuf = new char[32];
+            // 用于保存实际读取的字符数
+            int hasRead = 0;
+            // 使用循环来重复“取水”过程
+            while ((hasRead = fr.read(cbuf)) > 0) {
+                // 取出“竹筒”中水滴（字符），将字符数组转换成字符串输入！
+                System.out.print(new String(cbuf, 0, hasRead));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
---
+++ chapter99-15-3/src/main/java/com/goat/A03/FileWriterTest.java
@@ -0,0 +1,23 @@
+package com.goat.A03;
+
+
+import org.testng.annotations.Test;
+
+import java.io.*;
+
+public class FileWriterTest {
+
+    @Test
+    public void test(){
+        try(FileWriter fw = new FileWriter("poem.txt")){
+            fw.write("锦瑟 - 李商隐\r\n");
+            fw.write("锦瑟无端五十弦，一弦一柱思华年。\r\n");
+            fw.write("庄生晓梦迷蝴蝶，望帝春心托杜鹃。\r\n");
+            fw.write("沧海月明珠有泪，蓝田日暖玉生烟。\r\n");
+            fw.write("此情可待成追忆，只是当时已惘然。\r\n");
+        }
+        catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
