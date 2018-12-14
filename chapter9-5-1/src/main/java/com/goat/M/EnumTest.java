cb5c02f0851721d250076d303e44e76b4cc043be
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-12-14 17:39:33 +0800

    增加 Java设计模式  枚举类 代替多重 if...else

---
+++ chapter9-5-1/src/main/java/com/goat/M/EnumTest.java
@@ -0,0 +1,49 @@
+package com.goat.M;
+
+/**
+ * Created by 64274 on 2018/12/14.
+ *
+ * @ Description: 使用枚举类 中的 抽象方法 实现 替换多重 if...else 语句
+ * @ author  山羊来了
+ * @ date 2018/12/14---16:25
+ */
+public enum EnumTest {  //在枚举里面定义一个抽象方法，每种类型对应各自的具体实现。
+
+    ONE("one") {
+        @Override
+        public void apply() {
+            System.out.println("one");
+        }
+    },
+    TWO("two") {
+        @Override
+        public void apply() {
+            System.out.println("two");
+        }
+    },
+    THREE("three") {
+        @Override
+        public void apply() {
+            System.out.println("three");
+        }
+    },
+    FOUR("four") {
+        @Override
+        public void apply() {
+            System.out.println("four");
+        }
+    };
+
+    public abstract void apply();
+
+    private String type;
+
+    EnumTest(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
---
+++ chapter9-5-1/src/main/java/com/goat/M/TestNG.java
@@ -0,0 +1,22 @@
+package com.goat.M;
+
+
+import org.testng.annotations.Test;
+
+
+public class TestNG {
+
+
+    @Test
+    public void test1() {
+
+    }
+    // 枚举测试
+    @Test
+    public void testEnum() {
+//        String input = "one";
+//        String input = "12"; // java.lang.IllegalArgumentException: No enum constant com.goat.M.EnumTest.12
+//        EnumTest.valueOf(input.toUpperCase()).apply();
+    }
+
+}
