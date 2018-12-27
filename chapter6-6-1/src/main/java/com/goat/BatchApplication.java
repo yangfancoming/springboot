3866f3f1142d51e1f2e7d83a2bffdbbef57283ef
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2018-12-27 15:14:52 +0800

    增加 springboot batch 批处理 demo

---
+++ chapter6-6-1/src/main/java/com/goat/BatchApplication.java
@@ -0,0 +1,18 @@
+package com.goat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+     * @Description: 功能描述：(这里用一句话描述这个方法的作用)
+     * @author: 杨帆
+     * @Param: 
+     * @Return: 
+     * @Date:   2018/12/27
+*/
+@SpringBootApplication
+public class BatchApplication {
+    public static void main(String[] args)  {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
---
+++ chapter6-6-1/src/main/java/com/goat/processor/RecordProcessor.java
@@ -0,0 +1,28 @@
+package com.goat.processor;
+
+
+import com.goat.model.RecordSO;
+import com.goat.model.WriterSO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class RecordProcessor implements ItemProcessor<RecordSO, WriterSO> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecordProcessor.class);
+
+    @Override
+    public WriterSO process(RecordSO item) {
+        LOGGER.info("Processing Record: {}", item);
+        WriterSO writerSo = new WriterSO();
+        writerSo.setId(item.getId());
+        writerSo.setFullName(item.getFirstName() + " " + item.getLastName());
+        String substring = String.valueOf(Math.random()).substring(3, 8);
+        if(substring.contains("0")){
+            return null; // 返回空，即词条不插入
+        }
+        writerSo.setRandomNum(substring);
+        LOGGER.info("Processed Writer: {}", writerSo);
+        return writerSo;
+    }
+}
