3abd0ce3f5d1c191e53aa897b669f83a8d157c5a
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-01-04 14:53:31 +0800

    整理项目 去掉无用文件 和 增加注释

--- chapter5-3-8/src/main/java/com/goat/service/TestServiceImpl.java
+++ chapter5-3-8/src/main/java/com/goat/service/TestServiceImpl.java
@@ -10,7 +10,11 @@ import com.goat.entity.User;
 //@Service(version = "1.0.0", timeout = 60000)
 @Service
 public class TestServiceImpl extends ServiceImpl<UserMapper, User> implements ITestService {
+
+
+    // doit  @Transactional 事务回滚测试
     @Override
+
     public User sayHello(Integer id) {
         System.out.println("进入Dubbo 远程调用。。。。。。");
         return baseMapper.selectById(id);
