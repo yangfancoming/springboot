1f9c11acca4a59ff9acda60d25aa913d51990f3d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-01-07 18:04:02 +0800

    整理 mysql 代码积累

--- chapter9-1-1/src/main/java/com/goat/temp.sql
+++ chapter9-1-1/src/main/java/com/goat/temp.sql
@@ -1,13 +1,14 @@
-﻿show VARIABLES like '%max_allowed_packet%';
+﻿delete from t_official_sys_user where USER_NAME IN
+                                      ( SELECT USER_NAME FROM t_official_sys_user b group by b.`USER_NAME` having  count(1) > 1) ;
 
-USE test;
-SELECT * FROM ews_grade;
+#   [Err] 1093 - You can't specify target table 't_official_sys_user' for update in FROM clause
+delete from t_official_sys_user where USER_NAME IN(
+  select USER_NAME from
+                        ( SELECT USER_NAME FROM t_official_sys_user b group by b.`USER_NAME` having  count(1) > 1)
+   as temtable );
 
-SELECT e_date  FROM ews_grade GROUP BY e_date HAVING e_result = 'yes'
+# 分析：先把要删除的目标放到一张临时表再把要删除的条件指定到这张临时表即可。
+#       不能在同一表中查询的数据作为同一表的更新数据。
+#       出现以上错误，是因为想将表自身的字段A的值作为被更新字段B的值而导致的。
 
-
-
-;
-
-SELECT * FROM ews_grade WHERE e_result = 'yes';
 
