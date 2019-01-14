848bf25416f93c18d039c642147e6c2a12792a34
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-01-14 21:50:22 +0800

    增加 topic 示例及模块整理、代码重构

---
+++ chapter8-3-0/src/test/java/com/goat/topic/Recv2.java
@@ -0,0 +1,32 @@
+package com.goat.topic;
+
+import com.goat.CommonTest;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+/**
+ * 消费者2
+ */
+public class Recv2 extends CommonTest {
+
+    public static void main(String[] argv) throws Exception {
+
+        Channel channel = init2();
+        // 声明队列
+        channel.queueDeclare(QUEUE2, false, false, false, null);
+        // 绑定队列到交换机，同时指定需要订阅的routing key。订阅 insert、update、delete
+        channel.queueBind(QUEUE2, TOPIC_EXCHANGE_NAME, "item.*");
+        // 定义队列的消费者
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override // 获取消息，并且处理，这个方法类似事件监听，如果有消息的时候，会被自动调用
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties,byte[] body) {
+                String msg = new String(body);// body 即消息体
+                System.out.println(" [消费者2] received : " + msg + "!");
+            }
+        };
+        // 监听队列，自动ACK
+        channel.basicConsume(QUEUE2, true, consumer);
+    }
+}
---
+++ chapter8-3-0/src/test/java/com/goat/topic/Send.java
@@ -0,0 +1,21 @@
+package com.goat.topic;
+
+import com.goat.CommonTest;
+import com.rabbitmq.client.Channel;
+
+public class Send extends CommonTest {
+
+    public static void main(String[] argv) throws Exception {
+        Channel channel = init();
+        // 声明exchange，指定类型为fanout
+        channel.exchangeDeclare(TOPIC_EXCHANGE_NAME, "direct");
+        // 消息内容
+        String message = "商品新增了， id = 1001";
+        // 发布消息到Exchange
+//        channel.basicPublish(DIRECT_EXCHANGE_NAME, "insert", null, message.getBytes()); // 只有 recv1 可以接收到
+        channel.basicPublish(TOPIC_EXCHANGE_NAME, "item.insert", null, message.getBytes()); //  recv1 和 recv2 都可以接收到
+        System.out.println(" [商品服务：] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+    }
+}
