d50f79fbce0b48f868fa280b28b93eeceb2cf013
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-01-16 13:49:40 +0800

    增加 kafka  示例  成功收发数据

--- chapter8-3-3/src/main/java/com/goat/MessageConsumer.java
+++ chapter8-3-3/src/main/java/com/goat/MessageConsumer.java
@@ -1,25 +1,25 @@
-//package com.goat;
-//
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * 类的功能描述：
-// * 消息消费者者用于处理消息
-// *
-// * @ClassName: MessageConsumer
-// * @Author haichen
-// * @Date 2017-09-13 03:45:55
-// */
-//@Service
-//public class MessageConsumer {
-//    private static Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
-//
-//    @KafkaListener(topics = "test")
-//    public void onMessage(String payMessage) {
-//        logger.info("MessageConsumer: onMessage: message is: [" + payMessage + "]");
-//    }
-//}
+package com.goat;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * 类的功能描述：
+ * 消息消费者者用于处理消息
+ *
+ * @ClassName: MessageConsumer
+ * @Author haichen
+ * @Date 2017-09-13 03:45:55
+ */
+@Service
+public class MessageConsumer {
+    private static Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @KafkaListener(topics = "test")
+    public void onMessage(String payMessage) {
+        logger.info("MessageConsumer: onMessage: message is: [" + payMessage + "]");
+    }
+}
