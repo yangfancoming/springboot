ed0658693098c1be833213e0dda4de1717785134
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-02-24 14:39:04 +0800

    增加 springboot  集成  HttpSession 示例

--- chapter2-0-4/README.md
+++ chapter2-0-4/README.md
@@ -1,6 +1,21 @@
 # Session： 
+    实现原理： Session的实现 是依赖于 cookie 的
     概念： 服务器端会话技术,在一次会话的多次请求间 共享数据，将数据保存在服务器端的对象中。
     需要注意的是：
     @WebServlet(name = "sessionDemo01",urlPatterns = "/sessionDemo01") // 若没有设置 @WebServlet 的name属性，默认值会是Servlet的类完整名称
     如果不明确写出属性 比如这样：@WebServlet("fuck")   这将是 代表 urlPatterns 的属性  而不是 name 属性
     因为在 @WebServlet 注解的源码中 可以看到   * E.g. <code>@WebServlet("/path")}<br>  就代表了 他是 urlPatterns 的属性
+    
+# 细节
+    1.当客户端(浏览器)关闭后，服务器不关闭，两次获取的session 并不是同一个！  （默认情况下）
+        解决方法： Cookie cookie = new Cookie("JSESSIONID",session.getId());
+    2.当客户端(浏览器)不关闭，服务器关闭，两次获取的session 并不是同一个！
+        解决方法： 
+            session 钝化(序列化)： 在服务器正常关闭之前，将 session 对象 序列化到磁盘上
+            session 活化(烦序列化)： 在服务器启动后，将 session 文件 转换为内容中的 session 对象即可
+            
+        
+    3.session 销毁
+        1. 服务器关闭
+        2. session对象调用 session.invalidate();
+        3. session 的失效时间
--- chapter2-0-4/src/main/java/com/goat/session/SessionDemo03.java
+++ chapter2-0-4/src/main/java/com/goat/session/SessionDemo03.java
@@ -1,10 +1,7 @@
 package com.goat.session;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 
 /**
  * Created by 64274 on 2019/2/24.
@@ -13,8 +10,8 @@ import javax.servlet.http.HttpSession;
  * @ author  山羊来了
  * @ date 2019/2/24---11:38
  */
-@WebServlet(name = "sessionDemo02",urlPatterns = "/sessionDemo02")
-public class SessionDemo02 extends HttpServlet {
+@WebServlet(name = "sessionDemo03",urlPatterns = "/sessionDemo03")
+public class SessionDemo03 extends HttpServlet {
 
     //   http://localhost:8204/sessionDemo02
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
@@ -22,9 +19,13 @@ public class SessionDemo02 extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("进入 sessionDemo02...........doGet");
+        System.out.println("进入 sessionDemo03...........doGet");
         HttpSession session = request.getSession();
-        Object msg = session.getAttribute("msg");
-        System.out.println(msg.toString());
+
+        // 这样一来 就算 客户端关闭后 再打开客户端 进行请求 session 也能相同
+        Cookie cookie = new Cookie("JSESSIONID",session.getId());
+        cookie.setMaxAge(60*60); // 设置 cookie 最大存活时间
+        response.addCookie(cookie);
+
     }
 }
