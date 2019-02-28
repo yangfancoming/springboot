5dbe5a098be3fc1a32d0f74d2c3f87ea97adbff7
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-02-28 22:03:37 +0800

    增加  springboot 整合 Layui

---
+++ chapter2-6-8/src/main/java/com/goat/layui/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.goat.layui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+
+    // http://localhost:8268/hello/test1
+    @GetMapping("/test1")
+    public String test1(ModelMap map) {
+        map.addAttribute("hello", "asdkfjdsfkdddd");
+        return "index";
+    }
+
+    // http://localhost:8268/hello/test2
+    @GetMapping("/test2")
+    public String test2(ModelMap map) {
+        map.addAttribute("hello", "asdkfjdsfkdddd");
+        return "main";
+    }
+
+    // http://localhost:8268/hello/test3
+    @GetMapping("/test3")
+    public String test3() {
+        return "userList";
+    }
+    // http://localhost:8268/hello/test4
+    @GetMapping("/test4")
+    public String test4() {
+        return "roleList";
+    }
+}
---
+++ chapter9-8-3/README.md
@@ -0,0 +1,3 @@
+# LayUI
+    layui.all.js 里面包含了 lay 文件夹下 所有的模块 
+    如果使用lay文件夹下 具体的js模块  需要使用 use.('layer','laydate',function)
