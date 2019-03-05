a85c3b972558f601db10352d2d422bd94ff84fae
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-03-05 22:09:37 +0800

    优化代码 使用WebMvcConfigurer 取代 controller中的跳转

---
+++ chapter9-8-3/src/main/java/com/goat/config/MyMvcConfig.java
@@ -0,0 +1,34 @@
+package com.goat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // http://localhost:8983/hello/index
+        registry.addViewController("/index").setViewName("index");
+        // http://localhost:8983/hello/test1
+        registry.addViewController("/test1").setViewName("menu1");
+        // http://localhost:8983/login
+        registry.addViewController("/login").setViewName("login/login");
+        // http://localhost:8983/news
+        registry.addViewController("/news").setViewName("news/newsList");
+        // http://localhost:8983/newsAdd
+        registry.addViewController("/newsAdd").setViewName("news/newsAdd");
+        // http://localhost:8983/user
+        registry.addViewController("/user").setViewName("user/userList");
+        // http://localhost:8983/dept
+        registry.addViewController("/dept").setViewName("dept/dept");
+        // http://localhost:8983/menu/menu
+        registry.addViewController("/menu/menu").setViewName("menu/menu");
+        // http://localhost:8983/menu/menuAdd
+        registry.addViewController("/menu/menuAdd").setViewName("menu/menuAdd");
+    }
+
+}
+
