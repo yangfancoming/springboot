8260748487c45f20e8042480d9b3af14c56a5ab8
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-03-07 10:13:31 +0800

    增加 springboot kotlin 开发web  已OK

--- chapter8-0-7/src/main/kotlin/com/goat/KotlinjpaApplication.kt
+++ chapter8-0-7/src/main/kotlin/com/goat/KotlinjpaApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinjpaApplication
-
+//启动类
 fun main(args: Array<String>) {
     runApplication<KotlinjpaApplication>(*args)
 }
