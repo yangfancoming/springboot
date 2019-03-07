d9bba73ad72996b260ea0bcd8cd35b53dd5e7d35
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-03-07 17:57:13 +0800

    springboot  集成  Redis + Lua 限流  项目示例

---
+++ chapter2-9-5/src/main/java/com/goat/limit/annotation/RateLimit.java
@@ -0,0 +1,35 @@
+package com.goat.limit.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 描述: 限流注解
+ **/
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimit {
+
+    /**
+     * 限流唯一标示
+     *
+     * @return
+     */
+    String key() default "";
+
+    /**
+     * 限流时间
+     *
+     * @return
+     */
+    int time();
+
+    /**
+     * 限流次数
+     *
+     * @return
+     */
+    int count();
+}
