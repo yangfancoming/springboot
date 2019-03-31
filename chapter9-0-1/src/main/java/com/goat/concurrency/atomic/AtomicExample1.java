fd5fe0219a52017ac20971373aa62f23f8af89a2
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-03-31 22:26:42 +0800

    使用模板方法 设计模式 重构   干掉重复代码

--- chapter9-0-1/src/main/java/com/goat/concurrency/atomic/AtomicExample1.java
+++ chapter9-0-1/src/main/java/com/goat/concurrency/atomic/AtomicExample1.java
@@ -2,48 +2,25 @@ package com.goat.concurrency.atomic;
 
 
 import com.goat.concurrency.annoations.ThreadSafe;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.goat.concurrency.common.BaseExample;
+import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicLong;
 
 @ThreadSafe
-public class AtomicExample1 {
-
-    private static int threadTotal = 200;
-    private static int clientTotal = 5000;
+public class AtomicExample1 extends BaseExample {
 
     private static AtomicLong count = new AtomicLong(0);
 
-    public static void main(String[] args) throws InterruptedException {
-        final Logger myLog = LoggerFactory.getLogger(AtomicExample1.class);
-        ExecutorService exec = Executors.newCachedThreadPool();
-        final Semaphore semp = new Semaphore(threadTotal);
-        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
-        for (int index = 0; index < clientTotal; index++) {
-            exec.execute(() -> {
-                try {
-                    semp.acquire();
-                    add();
-                    semp.release();
-                } catch (Exception e) {
-                    myLog.error("exception", e);
-                }
-                countDownLatch.countDown();
-            });
-
-        }
-        countDownLatch.await(); // 这句代码 很关键 没有他  数量就是不对的。。。
-        exec.shutdown();
-        myLog.info("count:{}", count.get());
+    @Test
+    public void test1() throws InterruptedException {
+        test();
+        System.out.println(count.get());
     }
 
-    private static void add() {
+    @Override
+    public void add() {
         count.incrementAndGet();
-//        count.compareAndSet(1,2);
     }
+
 }
--- chapter9-0-1/src/main/java/com/goat/concurrency/common/BaseExample.java
+++ chapter9-0-1/src/main/java/com/goat/concurrency/common/BaseExample.java
@@ -1,5 +1,13 @@
 package com.goat.concurrency.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by 64274 on 2019/3/31.
  *
@@ -7,4 +15,35 @@ package com.goat.concurrency.common;
  * @ author  山羊来了
  * @ date 2019/3/31---22:07
  */
-public class BaseExample {}
+public abstract class BaseExample {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public void test() throws InterruptedException {
+        final Logger myLog = LoggerFactory.getLogger(getClass());
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semp = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int index = 0; index < clientTotal; index++) {
+            exec.execute(() -> {
+                try {
+                    semp.acquire();
+                    add();
+                    semp.release();
+                } catch (Exception e) {
+                    myLog.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+        countDownLatch.await(); // 这句代码 很关键 没有他  数量就是不对的。。。
+        exec.shutdown();
+    }
+
+    public abstract void add ();
+
+}
