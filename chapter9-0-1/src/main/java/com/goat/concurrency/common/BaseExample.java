302e01d6b4462ea7b451b6d07eb3aac8081368e7
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-03-31 22:28:37 +0800

    增加相关注释

--- chapter9-0-1/src/main/java/com/goat/concurrency/common/BaseExample.java
+++ chapter9-0-1/src/main/java/com/goat/concurrency/common/BaseExample.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Semaphore;
 /**
  * Created by 64274 on 2019/3/31.
  *
- * @ Description: TODO
+ * @ Description: 使用 模板方法 设计模式  防止重复代码
  * @ author  山羊来了
  * @ date 2019/3/31---22:07
  */
@@ -24,24 +24,23 @@ public abstract class BaseExample {
 
     public void test() throws InterruptedException {
         final Logger myLog = LoggerFactory.getLogger(getClass());
-        ExecutorService exec = Executors.newCachedThreadPool();
-        final Semaphore semp = new Semaphore(threadTotal);
+        ExecutorService exec = Executors.newCachedThreadPool();// 定义线程池
+        final Semaphore semp = new Semaphore(threadTotal); // 信号量 运行同时并发执行的线程数
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
         for (int index = 0; index < clientTotal; index++) {
             exec.execute(() -> {
                 try {
-                    semp.acquire();
+                    semp.acquire(); // 信号量 确认
                     add();
-                    semp.release();
+                    semp.release(); // 信号量 释放
                 } catch (Exception e) {
                     myLog.error("exception", e);
                 }
                 countDownLatch.countDown();
             });
-
         }
         countDownLatch.await(); // 这句代码 很关键 没有他  数量就是不对的。。。
-        exec.shutdown();
+        exec.shutdown(); // 关闭线程池
     }
 
     public abstract void add ();
