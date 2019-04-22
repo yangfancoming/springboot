f3394da59ff425558135961f11720f42259d0739
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-04-22 14:11:50 +0800

    学习 java 线程的状态

---
+++ chapter99-16-3/src/main/java/A02_lifecycle/README.md
@@ -0,0 +1,6 @@
+#  各种状态一目了然，值得一提的是"blocked"这个状态：
+    线程在Running的过程中可能会遇到阻塞(Blocked)情况
+    调用join()和sleep()方法，sleep()时间结束或被打断，join()中断,IO完成都会回到Runnable状态，等待JVM的调度。
+    调用wait()，使该线程处于等待池(wait blocked pool),直到notify()/notifyAll()，线程被唤醒被放到锁定池(lock blocked pool )，释放同步锁使线程回到可运行状态（Runnable）
+    对Running状态的线程加同步锁(Synchronized)使其进入(lock blocked pool ),同步锁被释放进入可运行状态(Runnable)。
+    此外，在runnable状态的线程是处于被调度的线程，此时的调度顺序是不一定的。Thread类中的yield方法可以让一个running状态的线程转入runnable。
--- chapter99-16-3/src/main/java/A02_lifecycle/TestNG4.java
+++ chapter99-16-3/src/main/java/A02_lifecycle/TestNG4.java
@@ -23,7 +23,7 @@ public class TestNG4 {
             wait(); // 使当前线程永久等待，直到其他线程调用notify()或notifyAll()方法
         }
 
-        public synchronized void notifyNow() throws InterruptedException {
+        public synchronized void notifyNow() {
             notify(); // 唤醒由调用wait()方法进入等待状态的线程
         }
 
@@ -45,20 +45,20 @@ public class TestNG4 {
         System.out.println("新建线程：" + thread.getState());// 输出线程状态
 
         thread.start(); // 调用thread对象的start()方法，启动新线程
-        System.out.println("启动线程：" + thread.getState());// 输出线程状态
+        System.out.println("启动线程：" + thread.getState());
 
         Thread.sleep(100); // 当前线程休眠0.1秒，使新线程运行waitForASecond()方法
-        System.out.println("计时等待：" + thread.getState());// 输出线程状态
+        System.out.println("计时等待：" + thread.getState());
 
 
         Thread.sleep(1000); // 当前线程休眠1秒，使新线程运行waitForYears()方法
-        System.out.println("等待线程：" + thread.getState());// 输出线程状态
+        System.out.println("等待线程：" + thread.getState());
 
 
         state.notifyNow(); // 调用state的notifyNow()方法
-        System.out.println("唤醒线程：" + thread.getState());// 输出线程状态
+        System.out.println("唤醒线程：" + thread.getState());
 
         Thread.sleep(1000); // 当前线程休眠1秒，使新线程结束
-        System.out.println("终止线程：" + thread.getState());// 输出线程状态
+        System.out.println("终止线程：" + thread.getState());
     }
 }
