edf1012dbce24e1b44cbebb9e15bfc49be23d4ac
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-04-22 13:09:32 +0800

    学习 java 8大排序方法

---
+++ chapter8-6-2/src/main/java/com/goat/ds/sort/App.java
@@ -0,0 +1,38 @@
+package com.goat.ds.sort;
+
+import org.junit.Test;
+
+/**
+ * Created by 64274 on 2019/4/22.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2019/4/22---12:53
+ */
+public class App {
+
+    int xx[] = {3,1,5,7,2,4,9,6,10,8};
+
+    @Test
+    public void test(){
+        bubbleSort.bubbleSort(xx);
+        System.out.println(xx);
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+    @Test
+    public void test3(){
+        bubbleSort.insertSort(xx);
+        System.out.println(xx);
+    }
+
+    @Test
+    public void test4(){
+        bubbleSort.twoInsertSort(xx);
+        System.out.println(xx);
+    }
+}
