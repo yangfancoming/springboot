94369b102c1ce25d5a137e09a69efaec58632676
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-04-25 11:38:49 +0800

    复习 druid 配置

--- chapter4-0-0/src/main/java/com/goat/config/DruidConfiguration.java
+++ chapter4-0-0/src/main/java/com/goat/config/DruidConfiguration.java
@@ -9,36 +9,39 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * ========================
- * 监控页面访问地址：127.0.0.1:8080/druid/login.html
+ * 监控页面访问地址：127.0.0.1:8400/druid/login.html
+ * 账号：druid
+ * 密码：123456
  * ========================
  */
 @Configuration
 public class DruidConfiguration {
 
+    /** 下面这个注入bean的方法 和新建一个类 这样配置 效果是一样的
+     @WebServlet(urlPatterns = "/druid/*", initParams = { @WebInitParam(name = "loginUsername", value = "sim"),
+     @WebInitParam(name = "loginPassword", value = "sim123"),
+     @WebInitParam(name = "resetEnable", value = "false")
+     public class DruidStatViewServlet extends StatViewServlet { }
+    */
     @Bean
     public ServletRegistrationBean statViewServlet(){
         //创建servlet注册实体
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-        //设置ip白名单
-        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
-        //设置ip黑名单，如果allow与deny共同存在时,deny优先于allow
-        servletRegistrationBean.addInitParameter("deny","192.168.0.19");
-        //设置控制台管理用户
-        servletRegistrationBean.addInitParameter("loginUsername","druid");
-        servletRegistrationBean.addInitParameter("loginPassword","123456");
-        //是否可以重置数据
-        servletRegistrationBean.addInitParameter("resetEnable","false");
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1"); //设置ip白名单
+        servletRegistrationBean.addInitParameter("deny","192.168.0.19"); //设置ip黑名单，如果allow与deny共同存在时,deny优先于allow
+        servletRegistrationBean.addInitParameter("loginUsername","druid");  //设置控制台管理 账号
+        servletRegistrationBean.addInitParameter("loginPassword","123456"); //设置控制台管理 密码
+        servletRegistrationBean.addInitParameter("resetEnable","false"); //是否可以重置数据
         return servletRegistrationBean;
     }
 
+
+
     @Bean
     public FilterRegistrationBean statFilter(){
-        //创建过滤器
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        //设置过滤器过滤路径
-        filterRegistrationBean.addUrlPatterns("/*");
-        //忽略过滤的形式
-        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());  //创建过滤器
+        filterRegistrationBean.addUrlPatterns("/*");  //设置过滤器过滤路径
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*"); //忽略过滤的形式
         return filterRegistrationBean;
     }
 }
