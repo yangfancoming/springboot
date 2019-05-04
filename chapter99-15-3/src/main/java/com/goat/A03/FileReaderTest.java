b70789b2dc2441dc2bc4b5af4cefa87a7308159d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-05-04 11:51:53 +0800

    复习 IO 流

--- chapter99-15-3/src/main/java/com/goat/A03/FileReaderTest.java
+++ chapter99-15-3/src/main/java/com/goat/A03/FileReaderTest.java
@@ -10,17 +10,12 @@ public class FileReaderTest {
 
     @Test
     public void test(){
-        try (
-                // 创建字符输入流
-                FileReader fr = new FileReader("E:\\Code\\J2EE_code\\MySpringBoot\\springboot\\chapter99-15-3\\src\\main\\java\\com\\goat\\A03\\FileWriterTest.java")) {
-            // 创建一个长度为32的“竹筒”
-            char[] cbuf = new char[32];
-            // 用于保存实际读取的字符数
-            int hasRead = 0;
-            // 使用循环来重复“取水”过程
-            while ((hasRead = fr.read(cbuf)) > 0) {
-                // 取出“竹筒”中水滴（字符），将字符数组转换成字符串输入！
-                System.out.print(new String(cbuf, 0, hasRead));
+        File file = new File("hello.txt");// 相较于当前Module
+        try (FileReader fr = new FileReader(file)) {
+            char[] cbuf = new char[32];   // 创建一个长度为32的“竹筒”
+            int hasRead;   // 用于保存实际读取的字符数
+            while (  (hasRead = fr.read(cbuf)) > 0  ) {   // 使用循环来重复“取水”过程
+                System.out.print(new String(cbuf, 0, hasRead)); // 取出“竹筒”中水滴（字符），将字符数组转换成字符串输入！
             }
         } catch (IOException ex) {
             ex.printStackTrace();
---
+++ chapter99-15-3/src/main/java/com/goat/README.md
@@ -0,0 +1,7 @@
+# 流的分类
+    按操作数据单位不同： 字节流 8bit 、 字符流 16bit
+    流向不同： 输入流、输出流   （针对内存）
+    
+                字节流     字符流
+    输入流：InputStream     Reader
+    输出流：OutputStream    Writer
