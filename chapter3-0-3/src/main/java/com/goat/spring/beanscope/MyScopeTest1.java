de20cc85599d607470d8b2df19c83b5a5a932bab
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-05-22 09:37:35 +0800

    学习 @PostConstruct 注解

--- chapter3-0-3/src/main/java/com/goat/spring/beanscope/MyScopeTest1.java
+++ chapter3-0-3/src/main/java/com/goat/spring/beanscope/MyScopeTest1.java
@@ -3,7 +3,6 @@ package com.goat.spring.beanscope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 
 @Component
 public class MyScopeTest1 {
@@ -11,8 +10,6 @@ public class MyScopeTest1 {
     @Autowired  MyScope myScope;
     @Autowired  MyScopePrototype myScopePrototype;
 
-
-    @PostConstruct
     public void sayMyScope() {
         System.out.println("hello,i am MyScopeTest1, my scope is " + myScope.toString());
         System.out.println("hello,i am MyScopeTest1, my myScopePrototype is " + myScopePrototype.toString());
