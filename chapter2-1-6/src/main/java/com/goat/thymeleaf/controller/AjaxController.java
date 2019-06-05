f97bfb082730087e8f03570ed4603230b9a12d8f
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-06-05 15:13:46 +0800

    增加 ajax 前后台交互 示例  数组接收 和  list<String> 接收

--- chapter2-1-6/src/main/java/com/goat/thymeleaf/controller/AjaxController.java
+++ chapter2-1-6/src/main/java/com/goat/thymeleaf/controller/AjaxController.java
@@ -1,7 +1,10 @@
 package com.goat.thymeleaf.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -17,8 +20,30 @@ public class AjaxController {
 
     //   http://localhost:8216/ajax/test2
     @RequestMapping("test2")
-    public void test1(String param1,String param2) {
+    public void test2(String param1,String param2) {
         System.out.println(param1);
         System.out.println(param2);
     }
+
+    //   http://localhost:8216/ajax/test3
+    @RequestMapping("test3")
+    public void test3(String[] ids) {
+        System.out.println(ids);
+    }
+
+    //   http://localhost:8216/ajax/test4
+    // value 必须与 前端参数名称对应 否则 无法接收
+    @RequestMapping("test4")
+    public void test4(String pn,@RequestParam(value = "ids[]", required = false) List<String> ids) {
+        System.out.println(pn);
+        System.out.println(ids);
+    }
+
+    //   http://localhost:8216/ajax/test5
+    //  No primary or default constructor found for interface java.util.List
+    @RequestMapping("test5")
+    public void test5(List<String> ids) {
+        System.out.println(ids);
+    }
+
 }
