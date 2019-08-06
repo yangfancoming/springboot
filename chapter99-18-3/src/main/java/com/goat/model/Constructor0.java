02bfd79a5ba67c1b7129c0e6b3b62ad04f830e29
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-08-06 16:20:40 +0800

    复习 反射相关 知识

---
+++ chapter99-18-3/src/main/java/com/goat/model/Constructor0.java
@@ -0,0 +1,31 @@
+package com.goat.model;
+
+/**
+ * Created by 64274 on 2019/8/6.
+ *
+ * @ Description: 不声明构造函数
+ * @ author  山羊来了
+ * @ date 2019/8/6---15:16
+ */
+public class Constructor0 {
+
+    int id;
+    String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
