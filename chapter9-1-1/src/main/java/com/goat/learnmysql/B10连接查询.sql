cfa62c8a5876c238ee50a07cb356d191ddea781f
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-09-29 16:40:51 +0800

    复习 mysql  左外连接

--- chapter9-1-1/src/main/java/com/goat/learnmysql/B10连接查询.sql
+++ chapter9-1-1/src/main/java/com/goat/learnmysql/B10连接查询.sql
@@ -14,8 +14,17 @@ SELECT suppliers.s_id, s_name,f_name, f_price
 FROM fruits INNER JOIN suppliers  ON fruits.s_id = suppliers.s_id;
 
 
+# 【例7.48】查询供应f_id= ‘a1’的水果供应商提供的其他水果种类，SQL语句如下：
 
+SELECT s.* From fruits f left join suppliers s  on f.s_id = s.s_id where f_id = 'a1';
 
+SELECT f1.f_id, f1.f_name
+FROM fruits AS f1, fruits AS f2
+WHERE f1.s_id = f2.s_id ;
+
+SELECT f1.*
+FROM fruits AS f1, fruits AS f2
+WHERE f1.s_id = f2.s_id AND f2.f_id = 'a1';;
 
 
 
