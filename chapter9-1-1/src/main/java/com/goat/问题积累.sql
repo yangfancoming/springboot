766065030e96df29b2f6b700c4f10b4f139ddbfe
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-10-03 17:16:20 +0800

    代码格式化

--- chapter9-1-1/src/main/java/com/goat/问题积累.sql
+++ chapter9-1-1/src/main/java/com/goat/问题积累.sql
@@ -4,8 +4,8 @@
 #   [Err] 1093 - You can't specify target table 't_official_sys_user' for update in FROM clause
 delete from t_official_sys_user where USER_NAME IN(
   select USER_NAME from
-                        ( SELECT USER_NAME FROM t_official_sys_user b group by b.`USER_NAME` having  count(1) > 1)
-   as temtable );
+    ( SELECT USER_NAME FROM t_official_sys_user b group by b.`USER_NAME` having  count(1) > 1)
+      as temtable );
 
 # 分析：先把要删除的目标放到一张临时表再把要删除的条件指定到这张临时表即可。
 #       不能在同一表中查询的数据作为同一表的更新数据。
