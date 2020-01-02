9d4eabef451d8108102f48657a2b31bf94b93cc1
fan.yang <642744551@qq.com>
2020-01-02 18:14:39 +0800

    增加 SQLTest 测试类

--- chapter0-0-9/goatool-db/src/test/java/cn/goatool/db/SQLTest.java
+++ chapter0-0-9/goatool-db/src/test/java/cn/goatool/db/SQLTest.java
@@ -17,15 +17,115 @@ public class SQLTest {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
+    // 测试 混合 风格
+    @Test
+    public void demonstrateMixedStyle() {
+        final String sql = new SQL() {{
+            SELECT("id, name");
+            FROM("PERSON A");
+            WHERE("name like ?").WHERE("id = ?");
+        }}.toString();
+        assertEquals("SELECT id, name\nFROM PERSON A\nWHERE (name like ? AND id = ?)", sql);
+    }
 
+    // 测试 流式 风格
+    @Test
+    public void demonstrateFluentStyle() {
+        final String sql = new SQL().SELECT("id, name").FROM("PERSON A") .WHERE("name like ?").WHERE("id = ?").toString();
+        assertEquals("SELECT id, name\nFROM PERSON A\nWHERE (name like ? AND id = ?)", sql);
+    }
+
+    // 测试 所有参数都 不满足条件的情况
+    @Test
+    public void missingAllParams() {
+        final String expected = "SELECT P.ID, P.USERNAME, P.PASSWORD, P.FIRST_NAME, P.LAST_NAME\nFROM PERSON P\nORDER BY P.LAST_NAME";
+        assertEquals(expected, example2(null, null, null));
+    }
+
+    // 测试 前两个参数 不满足条件的情况
+    @Test
+    public void missingFirstTwoParams() {
+        final String expected =
+                "SELECT P.ID, P.USERNAME, P.PASSWORD, P.FIRST_NAME, P.LAST_NAME\n" +
+                        "FROM PERSON P\n" +
+                        "WHERE (P.LAST_NAME like #lastName#)\n" +
+                        "ORDER BY P.LAST_NAME";
+        assertEquals(expected, example2(null, null, "c"));
+    }
+
+    // 测试 第一个参数 不满足条件的情况
+    @Test
+    public void missingFirstParam() {
+        final String expected =
+                "SELECT P.ID, P.USERNAME, P.PASSWORD, P.FIRST_NAME, P.LAST_NAME\n" +
+                        "FROM PERSON P\n" +
+                        "WHERE (P.FIRST_NAME like #firstName# AND P.LAST_NAME like #lastName#)\n" +
+                        "ORDER BY P.LAST_NAME";
+        assertEquals(expected, example2(null, "b", "c"));
+    }
+
+    // 测试  所有参数都 满足条件的情况
+    @Test
+    public void withAllParams() {
+        final String expected =
+                "SELECT P.ID, P.USERNAME, P.PASSWORD, P.FIRST_NAME, P.LAST_NAME\n" +
+                        "FROM PERSON P\n" +
+                        "WHERE (P.ID like #id# AND P.FIRST_NAME like #firstName# AND P.LAST_NAME like #lastName#)\n" +
+                        "ORDER BY P.LAST_NAME";
+        assertEquals(expected, example2("a", "b", "c"));
+    }
+
+
+    private static String example2(final String id, final String firstName, final String lastName) {
+        return new SQL() {{
+            SELECT("P.ID, P.USERNAME, P.PASSWORD, P.FIRST_NAME, P.LAST_NAME");
+            FROM("PERSON P");
+            if (id != null) {
+                WHERE("P.ID like #id#");
+            }
+            if (firstName != null) {
+                WHERE("P.FIRST_NAME like #firstName#");
+            }
+            if (lastName != null) {
+                WHERE("P.LAST_NAME like #lastName#");
+            }
+            ORDER_BY("P.LAST_NAME");
+        }}.toString();
+    }
+
+
+
+    // 测试 select语句的 可变长参数
+    @Test
+    public void variableLengthArgumentOnSelect() {
+        final String sql = new SQL() {{ SELECT("P.ID", "P.USERNAME"); }}.toString();
+        assertEquals("SELECT P.ID, P.USERNAME", sql);
+    }
+
+    // 测试 select语句的 可变长参数 + distinct
+    @Test
+    public void variableLengthArgumentOnSelectDistinct() {
+        final String sql = new SQL() {{ SELECT_DISTINCT("P.ID", "P.USERNAME"); }}.toString();
+        assertEquals("SELECT DISTINCT P.ID, P.USERNAME", sql);
+    }
+
+    // 测试 from 语句的 可变长参数
+    @Test
+    public void variableLengthArgumentOnFrom() {
+        final String sql = new SQL() {{  SELECT().FROM("TABLE_A a", "TABLE_B b"); }}.toString();
+        assertEquals("FROM TABLE_A a, TABLE_B b", sql);
+    }
+
+    // 测试 join 语句的 可变长参数
+    @Test
+    void variableLengthArgumentOnJoin() {
+        final String sql = new SQL() {{ SELECT().JOIN("TABLE_A b ON b.id = a.id", "TABLE_C c ON c.id = a.id"); }}.toString();
+        assertEquals("JOIN TABLE_A b ON b.id = a.id\nJOIN TABLE_C c ON c.id = a.id", sql);
+    }
 
     @Test
     public void deleteUsingLimit() {
-
-        final String sql = new SQL() {{
-            DELETE_FROM("test").WHERE("status = #{status}").LIMIT(20);
-        }}.toString();
-
+        final String sql = new SQL() {{ DELETE_FROM("test").WHERE("status = #{status}").LIMIT(20); }}.toString();
         assertEquals("DELETE FROM test\nWHERE (status = #{status}) LIMIT 20", sql);
     }
 
