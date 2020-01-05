5ebae2b197a4f4e20353b345fce2ce79888d643e
fan.yang <642744551@qq.com>
2020-01-05 14:35:45 +0800

    增加反射工具类 DefaultObjectFactoryTest 测试用例

--- chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/reflect/factory/DefaultObjectFactoryTest.java
+++ chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/reflect/factory/DefaultObjectFactoryTest.java
@@ -1,11 +1,9 @@
 package cn.goatool.core.reflect.factory;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Administrator on 2020/1/5.
@@ -38,6 +36,38 @@ public class DefaultObjectFactoryTest {
         map.put("key", "value");
         map.put("key1", "value1");
         System.out.println(map);
+    }
+
+
+    @Test
+    public void creatHashMap() {
+        Map  map = defaultObjectFactory.create(Map.class,null,null);
+        Assert.assertTrue("Should be HashMap",map instanceof HashMap);
+    }
+
+    // 测试 通过 List Collection Iterable 接口创建的均为 ArrayList
+    @Test
+    public void createArrayList() {
+        List list = defaultObjectFactory.create(List.class);
+        Assert.assertTrue(" list should be ArrayList",list instanceof ArrayList);
+
+        Collection collection = defaultObjectFactory.create(Collection.class);
+        Assert.assertTrue(" collection should be ArrayList",collection instanceof ArrayList );
+
+        Iterable iterable = defaultObjectFactory.create(Iterable.class);
+        Assert.assertTrue(" iterable should be ArrayList",iterable instanceof ArrayList );
+    }
+
+    @Test
+    void createTreeSet() {
+        SortedSet sortedSet = defaultObjectFactory.create(SortedSet.class);
+        Assert.assertTrue(" sortedSet should be TreeSet",sortedSet instanceof TreeSet );
+    }
+
+    @Test
+    void createHashSet() {
+        Set set = defaultObjectFactory.create(Set.class);
+        Assert.assertTrue(" set should be HashSet",set instanceof HashSet );
     }
 
 }
