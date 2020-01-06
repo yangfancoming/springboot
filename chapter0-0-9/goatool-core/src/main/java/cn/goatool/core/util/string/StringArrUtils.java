391fa6230567ed54f34d8866e2fbb3f53116860d
fan.yang <642744551@qq.com>
2020-01-06 13:56:24 +0800

    增加 字符串数组工具类及对应的测试用例

--- chapter0-0-9/goatool-core/src/main/java/cn/goatool/core/util/string/StringArrUtils.java
+++ chapter0-0-9/goatool-core/src/main/java/cn/goatool/core/util/string/StringArrUtils.java
@@ -3,15 +3,69 @@ package cn.goatool.core.util.string;
 import cn.goatool.core.util.ObjectUtils;
 import com.sun.istack.internal.Nullable;
 
+import java.util.*;
+
+import static cn.goatool.core.util.string.StringCheckUtils.hasLength;
+
 /**
  * Created by Administrator on 2020/1/6.
  *
- * @ Description: 字符串数据工具类
+ * @ Description: 字符串数组工具类（使用字符串数组的方便方法）
  * @ author  山羊来了
  * @ date 2020/1/6---10:58
  */
 public class StringArrUtils {
 
+
+    /**
+     * 删除字符串中指定的字符
+     * @param inString 待操作的字符串
+     * @param charsToDelete 要删除的字符集合
+     * eg:	StringUtils.deleteAny("Able was I ere I saw Elba", "I") ===> "Able was  ere  saw Elba"
+     */
+    public static String deleteAny(String inString, @Nullable String charsToDelete) {
+        if (!hasLength(inString) || !hasLength(charsToDelete)) {
+            return inString;
+        }
+        StringBuilder sb = new StringBuilder(inString.length());
+        for (int i = 0; i < inString.length(); i++) {
+            char c = inString.charAt(i);
+            if (charsToDelete.indexOf(c) == -1) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 干掉字符串数组中的重复元素
+     * 由于使用了 LinkedHashSet 数组保留其原始顺序
+     * @param array  待去重数组
+     * @return 去重后的数组，数组元素顺序不变
+     */
+    public static String[] removeDuplicateStrings(String[] array) {
+        if (ObjectUtils.isEmpty(array)) {
+            return array;
+        }
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(array));
+        return toStringArray(set);
+    }
+
+    /**
+     * 将指定字符串添加到指定数组中
+     * 返回一个新的数组 包含指定数组内容外加指定字符串
+     * @param array  添加到的目标数组
+     * @param str  待添加的字符串
+     */
+    public static String[] addStringToArray(@Nullable String[] array, String str) {
+        if (ObjectUtils.isEmpty(array)) {
+            return new String[] {str};
+        }
+        String[] newArr = new String[array.length + 1];
+        System.arraycopy(array, 0, newArr, 0, array.length);
+        newArr[array.length] = str;
+        return newArr;
+    }
 
     /**
      * 数组拼接
@@ -34,4 +88,41 @@ public class StringArrUtils {
         System.arraycopy(array2, 0, newArr, array1.length, array2.length);
         return newArr;
     }
+
+    /**
+     * 数组合并
+     * 将给定的两个数组 合并成一个，两个数组中重叠/相同的元素 只保留第一次出现的元素
+     * 原始数组中元素顺序保持不变
+     * @param array1 第一个数组 (原数组)
+     * @param array2 第二个数组
+     */
+    @Nullable
+    public static String[] mergeStringArrays(@Nullable String[] array1, @Nullable String[] array2) {
+        if (ObjectUtils.isEmpty(array1)) {
+            return array2;
+        }
+        if (ObjectUtils.isEmpty(array2)) {
+            return array1;
+        }
+
+        List<String> result = new ArrayList<>();
+        result.addAll(Arrays.asList(array1));
+        for (String str : array2) {
+            if (!result.contains(str)) {
+                result.add(str);
+            }
+        }
+        return toStringArray(result);
+    }
+
+    /**
+     * Copy the given {@link Collection} into a {@code String} array.
+     * The {@code Collection} must contain {@code String} elements only.
+     * @param collection the {@code Collection} to copy (potentially {@code null} or empty)
+     * @return the resulting {@code String} array
+     */
+    public static String[] toStringArray(@Nullable Collection<String> collection) {
+        return (collection != null ? collection.toArray(new String[0]) : new String[0]);
+    }
+
 }
--- chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/util/string/StringArrUtilsTest.java
+++ chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/util/string/StringArrUtilsTest.java
@@ -14,6 +14,55 @@ import static org.junit.Assert.*;
 public class StringArrUtilsTest {
 
     @Test
+    public void testDeleteAny() {
+        String inString = "Able was I ere I saw Elba";
+
+        String res = StringArrUtils.deleteAny(inString, "I");
+        assertTrue("Result has no Is [" + res + "]", res.equals("Able was  ere  saw Elba"));
+
+
+        // 删除字符串中的所有出现的字符
+        res = StringArrUtils.deleteAny(inString, "AeEba!");
+        assertTrue("Result has no Is [" + res + "]", res.equals("l ws I r I sw l"));
+
+
+        // 区分大小写
+        res = StringArrUtils.deleteAny(inString, "A!");
+        assertTrue("Result has no Is [" + res + "]", res.equals("ble was I ere I saw Elba"));
+
+        String mismatch = StringArrUtils.deleteAny(inString, "#@$#$^");
+        assertTrue("Result is unchanged", mismatch.equals(inString));
+
+        //  测试 特殊字符
+        String whitespace = "This is\n\n\n    \t   a messagy string with whitespace\n";
+        assertTrue("Has CR", whitespace.contains("\n"));
+        assertTrue("Has tab", whitespace.contains("\t"));
+        assertTrue("Has  sp", whitespace.contains(" "));
+        String cleaned = StringArrUtils.deleteAny(whitespace, "\n\t ");
+        assertTrue("Has no CR", !cleaned.contains("\n"));
+        assertTrue("Has no tab", !cleaned.contains("\t"));
+        assertTrue("Has no sp", !cleaned.contains(" "));
+        assertTrue("Still has chars", cleaned.length() > 10);
+    }
+
+    @Test
+    public void testRemoveDuplicateStrings() {
+        String[] input = new String[] {"myString2", "myString1", "myString2"};
+        input = StringArrUtils.removeDuplicateStrings(input);
+        assertEquals("myString2", input[0]);
+        assertEquals("myString1", input[1]);
+    }
+
+    @Test
+    public void testSortStringArray() {
+        String[] input = new String[] {"myString2"};
+        input = StringArrUtils.addStringToArray(input, "myString1");
+        assertEquals("myString2", input[0]);
+        assertEquals("myString1", input[1]);
+    }
+
+
+    @Test
     public void testConcatenateStringArrays() {
         String[] input1 = new String[] {"myString2"};
         String[] input2 = new String[] {"myString1", "myString2"};
@@ -26,6 +75,22 @@ public class StringArrUtilsTest {
         assertArrayEquals(input1, StringArrUtils.concatenateStringArrays(input1, null));
         assertArrayEquals(input2, StringArrUtils.concatenateStringArrays(null, input2));
         assertNull(StringArrUtils.concatenateStringArrays(null, null));
+    }
+
+
+    @Test
+    @Deprecated
+    public void testMergeStringArrays() {
+        String[] input1 = new String[] {"myString2"};
+        String[] input2 = new String[] {"myString1", "myString2"};
+        String[] result = StringArrUtils.mergeStringArrays(input1, input2);
+        assertEquals(2, result.length);
+        assertEquals("myString2", result[0]);
+        assertEquals("myString1", result[1]);
+
+        assertArrayEquals(input1, StringArrUtils.mergeStringArrays(input1, null));
+        assertArrayEquals(input2, StringArrUtils.mergeStringArrays(null, input2));
+        assertNull(StringArrUtils.mergeStringArrays(null, null));
     }
 
 }
