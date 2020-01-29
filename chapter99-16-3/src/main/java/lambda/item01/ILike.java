0779e7b973db4783010e04e0d92059b0af4e4c46
fan.yang <642744551@qq.com>
2020-01-29 17:36:46 +0800

    复习 lambda 表达式

---
+++ chapter99-16-3/src/main/java/lambda/item01/ILike.java
@@ -0,0 +1,12 @@
+package lambda.item01;
+
+/**
+ * Created by Administrator on 2020/1/29.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2020/1/29---17:33
+ */
+public interface ILike {
+    void lambda();
+}
---
+++ chapter99-16-3/src/main/java/lambda/item01/Like.java
@@ -0,0 +1,15 @@
+package lambda.item01;
+
+/**
+ * Created by Administrator on 2020/1/29.
+ *
+ * @ Description: 外部类
+ * @ author  山羊来了
+ * @ date 2020/1/29---17:33
+ */
+public class Like implements ILike {
+    @Override
+    public void lambda() {
+        System.out.println("i like lambda ");
+    }
+}
