a3f6dedbabb24ccc88b089e764e57f2e23b5d005
fan.yang <642744551@qq.com>
2020-02-06 19:15:38 +0800

    增加 测试用例

--- chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/reflect/TypeParameterResolverTest.java
+++ chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/reflect/TypeParameterResolverTest.java
@@ -1,6 +1,7 @@
 package cn.goatool.core.reflect;
 
 import cn.goatool.core.reflect.typeparam.Level0Mapper;
+import cn.goatool.core.reflect.typeparam.Level1Mapper;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
@@ -17,6 +18,11 @@ import static org.junit.Assert.assertEquals;
  */
 public class TypeParameterResolverTest {
 
+    /**
+     * 测试类型 无继承关系
+     * 无参
+     * 有返回值(包装类型)
+    */
     @Test
     public void testReturn_Lv0SimpleClass() throws Exception {
         Class<?> clazz = Level0Mapper.class;
@@ -25,5 +31,29 @@ public class TypeParameterResolverTest {
         assertEquals(Double.class, result);
     }
 
+    /**
+     * 测试类型  有继承关系
+     * 有参(包装类型)
+     * 无返回值
+     */
+    @Test
+    public void testReturn_SimpleVoid() throws Exception {
+        Class<?> clazz = Level1Mapper.class;
+        Method method = clazz.getMethod("simpleSelectVoid", Integer.class);
+        Type result = TypeParameterResolver.resolveReturnType(method, clazz);
+        assertEquals(void.class, result);
+    }
 
+    /**
+     * 测试类型  有继承关系
+     * 有参(原始类型)
+     * 有返回值
+     */
+    @Test
+    public void testReturn_SimplePrimitive() throws Exception {
+        Class<?> clazz = Level1Mapper.class;
+        Method method = clazz.getMethod("simpleSelectPrimitive", int.class);
+        Type result = TypeParameterResolver.resolveReturnType(method, clazz);
+        assertEquals(double.class, result);
+    }
 }
---
+++ chapter0-0-9/goatool-core/src/test/java/cn/goatool/core/reflect/typeparam/Level1Mapper.java
@@ -0,0 +1,5 @@
+
+package cn.goatool.core.reflect.typeparam;
+
+public interface Level1Mapper<E, F> extends Level0Mapper<E, F, String> {
+}
