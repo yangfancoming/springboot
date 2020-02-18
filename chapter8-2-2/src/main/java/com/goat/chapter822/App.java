0a246410dac643bed10c6de6c5265cf6e3383b4f
fan.yang <642744551@qq.com>
2020-02-18 16:16:55 +0800

    MinIo 增加目录创建 示例

--- chapter8-2-2/src/main/java/com/goat/chapter822/App.java
+++ chapter8-2-2/src/main/java/com/goat/chapter822/App.java
@@ -5,6 +5,10 @@ import io.minio.errors.InvalidEndpointException;
 import io.minio.errors.InvalidPortException;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.UUID;
+
 /**
  * Created by Administrator on 2020/1/10.
  *
@@ -23,9 +27,8 @@ public class App {
         try {
             // 检查文件夹是否已经存在
             boolean isExist = minioClient.bucketExists("managertest");
-            if(isExist)  return;
             // 创建一个名为managertest的文件夹
-            minioClient.makeBucket("managertest");
+            if (!isExist)  minioClient.makeBucket("managertest");
             // 使用putObject上传一个文件到文件夹中。
             //参数为：文件夹，要存成的名字，要存的文件
             minioClient.putObject("managertest","1.png", "D:\\123\\12.txt");
@@ -46,5 +49,30 @@ public class App {
         } catch (Exception e) {
             System.out.println("错误: " + e);
         }
+    }
+
+    @Test
+    public void uploadFile() throws Exception {
+        if (!minioClient.bucketExists("test")) {	//是否存在名为“test”的bucket
+            minioClient.makeBucket("test");
+        }
+        InputStream inputStream = new FileInputStream("D:\\123\\student.xml");
+
+        String newName ="pic/"+ UUID.randomUUID().toString().replaceAll("-", "") + ".xml";
+
+        //新的名称，pic会是bucket下的文件夹
+       //获取file的inputStream
+        minioClient.putObject("test", newName, inputStream, "application/octet-stream");	//上传
+        inputStream.close();
+        String url = minioClient.getObjectUrl("test", newName);	//文件访问路径
+        System.out.println(url); // http://192.168.211.128:9000/test/pic/26c86c2e915d4b5986aaf673b11c78fd.xml
+
+    }
+
+    @Test
+    public void deleteFile() throws Exception {
+//        String objectName = url.replaceAll("http://***.***.**.**:9000/test/", "http://192.168.211.128:9000/test/pic/26c86c2e915d4b5986aaf673b11c78fd.xml");
+        String objectName = "/pic/05e80193a7c745c59f5f37c23db95212.xml";
+        minioClient.removeObject("test", objectName);
     }
 }
