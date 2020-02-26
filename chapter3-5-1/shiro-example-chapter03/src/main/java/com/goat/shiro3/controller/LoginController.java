9560ff82163f6424791b9152ba1e6cbd188f15f2
fan.yang <642744551@qq.com>
2020-02-26 14:40:22 +0800

    复习 shiro

--- chapter3-5-1/shiro-example-chapter03/src/main/java/com/goat/shiro3/controller/LoginController.java
+++ chapter3-5-1/shiro-example-chapter03/src/main/java/com/goat/shiro3/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.stereotype.Controller;
