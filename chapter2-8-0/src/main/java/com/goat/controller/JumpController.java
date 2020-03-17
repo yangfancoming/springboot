c3345d0c7ad8f3179969c942785125f88308d48a
fan.yang <642744551@qq.com>
2020-03-17 12:38:32 +0800

    项目整合 干掉多余项目

--- chapter2-8-0/src/main/java/com/goat/controller/JumpController.java
+++ chapter2-8-0/src/main/java/com/goat/controller/JumpController.java
@@ -10,6 +10,7 @@ public class JumpController {
    // http://localhost:8280/jump?page=index   首页 柱状图
    // http://localhost:8280/jump?page=pie01   饼图
    // http://localhost:8280/jump?page=jquery  jQuery学习
+   // http://localhost:8280/jump?page=baiduMap  百度地图
     @RequestMapping("/jump")
     public String hello1(String page)  {
         return page;
