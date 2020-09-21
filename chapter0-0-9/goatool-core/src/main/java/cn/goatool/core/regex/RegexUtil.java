4445d1fbe496b93809e3598ed848498f98fd5897
yangfan <642744551@qq.com>
2020-09-21 10:33:55 +0800

    代码格式优化

--- chapter0-0-9/goatool-core/src/main/java/cn/goatool/core/regex/RegexUtil.java
+++ chapter0-0-9/goatool-core/src/main/java/cn/goatool/core/regex/RegexUtil.java
@@ -7,6 +7,6 @@ package cn.goatool.core.regex;
  * @ author  山羊来了
  * @ date 2020/3/4---10:05
  *
- *  查找替换所有多余空白行  学习IDEA的正则表达式 匹配查询    ^\s*\n   或者  ^[ \t]*\n
+ *  查找替换所有多余空白行 | 隔行干掉 | 空行  学习IDEA的正则表达式 匹配查询    ^\s*\n   或者  ^[ \t]*\n
  */
 public class RegexUtil {}
