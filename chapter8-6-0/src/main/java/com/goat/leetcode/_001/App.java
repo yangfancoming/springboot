31d0e623f099b89410299541c77a66a8afbffe90
yangfan <642744551@qq.com>
2021-06-21 15:32:50 +0800

    复习 LeetCode 第一题

--- chapter8-6-0/src/main/java/com/goat/leetcode/_001/App.java
+++ chapter8-6-0/src/main/java/com/goat/leetcode/_001/App.java
@@ -4,15 +4,16 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class App {
     /**
      *   给定一个整型数组和一个整数 target 如果数组中 任意两个元素之和 等于 target 则返回 两个元素的索引
      *   思路：
-     *   反向思维 相当于 将一维数组转成map集合   key 为 差值 value 为 数组索引 然后对map进行遍历
+     *   反向思维 相当于 将一维数组转成map集合   key 为 差值， value 为 数组索引 然后对数组遍历时每次都检测map是否已经存在，如果存在则匹配完成。
      *   一维数组 {2, 8, 11, -2}  target = 9
-     *   转换为map  {7, 1, -2, ok}
+     *   转换为map  {7, 1, -2, ok}     当数组遍历到 最后一个-2时，map中已经存在了-2，则匹配完成
     */
 
     @Test
@@ -30,12 +31,12 @@ public class App {
     }
 
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
             if (map.containsKey(nums[i])) {
                 return new int[]{map.get(nums[i]), i};
             }
-            map.put(target - nums[i], i); //  key 为 差值 value 为 数组索引
+            map.put(target - nums[i], i); //  key 为 差值， value 为 数组索引
         }
         return null;
     }
